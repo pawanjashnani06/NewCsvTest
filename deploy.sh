dc2e445e06133a62a8cb0b9bb0bab983ecf5db26
Abhishek Nalwaya <Abhishek_Nalwaya@mckinsey.com>
2017-07-13 15:18:30 +0530

    (chor) adding deploy task

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# more bash-friendly output for jq
+JQ="jq --raw-output --exit-status"
+
+configure_aws_cli(){
+	aws --version
+	aws configure set default.region ap-southeast-1
+	aws configure set default.output json
+}
+
+
+push_ecr_image(){
+	eval $(aws ecr get-login --no-include-email --region ap-southeast-1)
+	docker tag greenbankstub $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/greenbankstub:$CIRCLE_SHA1
+	docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/greenbankstub:$CIRCLE_SHA1
+}
+
+configure_aws_cli
+push_ecr_image
